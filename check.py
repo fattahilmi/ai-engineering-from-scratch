c7733ad5918c5d4c230b391d50bcaca5bd078ba4
Fattah Ilmi <fattahilmii@gmail.com>
2026-09-11 14:37:08 +0700

    feat: add version of torch

--- check.py
+++ check.py
@@ -4,3 +4,5 @@ print(f"MPS available: {torch.backends.mps.is_available()}")
 
 if torch.cuda.is_available():
     print(f"GPU: {torch.cuda.get_device_name(0)}")
+
+print(f"Version {torch.__version__}")
